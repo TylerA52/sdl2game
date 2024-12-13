2059b3af5c63b81c1595518d707b951c3fd76ad8
TylerA52 <tyleradams952@gmail.com>
2024-12-13 12:12:10 -0500

    adding a tilemp - doesn't work properly yet

---
+++ main/tilemap.cpp
@@ -0,0 +1,33 @@
+#include "tilemap.h"
+#include "render_window.h"
+#include <iostream>
+
+Tilemap::Tilemap(int mapWidth, int mapHeight, SDL_Texture* tileSet)
+    : mapWidth(mapWidth), mapHeight(mapHeight), tileSet(tileSet) {
+
+    map.resize(mapHeight, std::vector<int>(mapWidth, 0));
+
+    map = {
+        {11, 11, 11, 11},
+        {11, 11, 11, 11}
+    };
+}
+
+
+void Tilemap::renderMap(RenderWindow& window) {
+const int tilesPerRow = 8;
+
+    for (int y = 0; y < mapHeight; y++){
+        for (int x = 0; x < mapWidth; x++) {
+            int tileIndex = map[y][x];
+
+            if (tileIndex == 0) continue;
+
+            int s_x = tileIndex % tilesPerRow;
+            int s_y = tileIndex / tilesPerRow;
+
+            Entity tileEntity(s_x, s_y, x, y, tileSet);
+            window.render(tileEntity);
+        }
+    }
+}
---
+++ main/tilemap.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <vector>
+#include <SDL2/SDL.h>
+#include "render_window.h"
+
+class Tilemap {
+    public:
+        Tilemap(int mapWidth, int mapHeight, SDL_Texture* tileSet);
+        void renderMap(RenderWindow& window);
+    private:
+        int mapWidth;
+        int mapHeight;
+        SDL_Texture* tileSet;
+        std::vector<std::vector<int>> map;
+};
